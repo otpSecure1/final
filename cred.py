730dc53e9c593532dabfdce4af4caaae574a264e
otpSecure1 <125118880+otpSecure1@users.noreply.github.com>
2023-02-11 16:45:39 +0530

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,17 +1,2 @@
-import os
-
-#Twilio Details
-account_sid = os.environ.get(account_sid)
-auth_token = os.environ.get(auth_token)
-twilionumber = os.environ.get(twilionumber)
-twiliosmsnumber = os.environ.get(twiliosmsnumber)
-
-#FC Bot
-API_TOKEN = os.environ.get(API_TOKEN)
-
-#Host URL
-callurl = os.environ.get(callurl)
-twiliosmsurl = os.environ.get(twiliosmsurl)
-
-
-
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['account_sid'],os.environ['auth_token'],os.environ['twilionumber'],os.environ['twiliosmsnumber'],os.environ['API_TOKEN'],os.environ['callurl'], os.environ['twiliosmsurl'])
